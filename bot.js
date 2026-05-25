012f129aa1a44358417a8554c90693f7b0374916
mabd88537-web <mabd88537@gmail.com>
2026-05-25 08:19:22 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,9 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, 'البوت شغال يا معلم 🚀');
+});
+
+console.log('Bot is running...');
